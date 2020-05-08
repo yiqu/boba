5302d7260b891999c51eae543cdb68968a0208b0
Kevin Qu <kevinqu88@gmail.com>
2020-05-08 15:20:33 -0400

    auth guard finally

--- src/app/shared/route-guards/auth-guards.service.ts
+++ src/app/shared/route-guards/auth-guards.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRoute, ActivatedRouteSnapshot,
   RouterStateSnapshot, UrlTree, CanActivateChild } from '@angular/router';
-import { Observable, combineLatest, Subject } from 'rxjs';
+import { Observable, combineLatest, Subject, of, from } from 'rxjs';
 import { map, take, tap, skip } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import { VerifiedUser } from '../models/user.model';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -40,16 +42,16 @@ export class AuthUserExistGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean> | boolean | UrlTree {
 
-    return this.as.currentUser$
-    .pipe(
-      take(1),
-      map((val: VerifiedUser) => {
-        if (val) {
-          return this.router.createUrlTree(['/', 'my-account']);;
-        }
-        return true;
-      })
-    );
+    // return this.as.currentUser$
+    // .pipe(
+    //   take(1),
+    //   map((val: VerifiedUser) => {
+    //     if (val) {
+    //       return this.router.createUrlTree(['/', 'my-account']);;
+    //     }
+    //     return true;
+    //   })
+    // );
 
     // return combineLatest(this.as.currentUser$, this.as.authStateResolved$).pipe(
     //   take(1),
@@ -63,6 +65,33 @@ export class AuthUserExistGuard implements CanActivate {
     //   tap(() => {
     //   })
     // );
-  }
 
+
+    let res: Subject<any> = new Subject<any>();
+    firebase.auth().onAuthStateChanged(
+      (user: firebase.User) => {
+        if (user) {
+          const u = (<VerifiedUser>user.toJSON());
+          res.next(false);
+          return this.router.createUrlTree(['/', 'my-account']);
+        } else {
+          res.next(true);
+          return true;
+        }
+      },
+      (err) => {
+      }
+    );
+
+    return res.pipe(
+      take(1),
+      map((val) => {
+        if (val) {
+          return true;
+        } else {
+          return this.router.createUrlTree(['/', 'my-account']);
+        }
+      })
+    )
+  }
 }
